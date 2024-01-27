5e7b866eb3e920343d1f35c5a5274d0509409637
Christopher Wiafe Debrah <cd.jobs.w@gmail.com>
2024-01-27 17:59:54 +0000

    scripted some behaviours for the user form

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const btnAgreeYes = document.getElementById('btn-yes');
 const btnAgreeNo = document.getElementById('btn-no');
 const yesInfo = document.querySelector('.yes-info');
-console.log(btnAgreeYes.id);
+
+const userName = document.getElementById('username');
+const userGender = document.getElementById('gender');
+const userLevel = document.getElementById('level');
+const yesBtn = document.getElementById('submit-user-info');
+
+
+const userInfoChallenge = [];
 
 const containerInputInteractions = document.querySelector('.container-input-interactions');
 
@@ -14,21 +21,50 @@ const btnClicked = $('.btn').click(function (e){
         yesInfo.style.flexDirection = "column"; 
         yesInfo.style.justifyContent = "space-between"; 
         yesInfo.style.transition =  "all 0.4s";
+        changeNoBtnToClear();
         containerInputInteractions.style.backgroundColor = '#E5E5E5';
         newDiv.appendChild(yesInfo);
         containerInputInteractions.appendChild(newDiv);
         console.log("yes pressed");
     }
     else {
-        document.querySelector('.container-input-interactions').innerHTML = " ";
-        btnAgreeYes.style.visibility = "hidden";
-        noBtnDetails();
-        setTimeout(function(){
-            btnAgreeYes.style.visibility = "visible";
-        },9000);
-        
+        if(btnID === btnAgreeYes.id){
+            document.querySelector('.container-input-interactions').innerHTML = " ";
+            btnAgreeYes.style.visibility = "hidden";
+            noBtnDetails();
+            setTimeout(function(){
+                btnAgreeYes.style.visibility = "visible";
+            },9000);
+        }
+        else {
+            userName.placeholder = "Enter name here.."
+            // userName.value = " ";
+            userGender.value = "Male";
+            userLevel.value = "beginner";
+            btnAgreeNo.innerText = "No";
+            btnAgreeNo.style.backgroundColor = "#a05656";
+            console.log(this);
+
+            this.addEventListener("click", function(e) {
+               switchBtnFunction();
+            })
+            
+        } 
     }
 });
+
+function switchBtnFunction() {
+    if(userName.value === " "){
+        document.querySelector('.container-input-interactions').innerHTML = " ";
+        noBtnDetails ();
+    }
+}
+
+function changeNoBtnToClear() {
+    btnAgreeNo.innerText = "Clear";
+    btnAgreeNo.style.backgroundColor = "#3A3A3A";
+
+}
 
 
 const emElement = document.createElement('em');
@@ -57,3 +93,18 @@ function soundPlay(){
     lafter = new Audio("./sounds/laughter.mp3");
     lafter.play();
 }
+
+yesBtn.addEventListener("click", function() {
+   const userNameReal = userName.value;
+   const userGenderReal = userGender.value;
+   const userLevelReal = userLevel.value;
+
+   userInfoChallenge.push({
+    Username: userNameReal,
+    Gender: userGenderReal,
+    Level: userLevelReal
+   })
+
+   console.log(userInfoChallenge);
+
+});
